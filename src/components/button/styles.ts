5271432518fa37bc877b06db9c1f993da4782e41
Emanuel Tavecia <emanueltavecia@hotmail.com>
2024-12-10 21:20:37 -0300

    fix button shadow on android

--- src/components/button/styles.ts
+++ src/components/button/styles.ts
@@ -1,5 +1,5 @@
 import { colors, fontFamily } from '@/styles/theme'
-import { StyleSheet } from 'react-native'
+import { Platform, StyleSheet } from 'react-native'
 
 export const styles = StyleSheet.create({
   container: {
@@ -11,6 +11,15 @@ export const styles = StyleSheet.create({
     justifyContent: 'center',
     flexDirection: 'row',
     gap: 14,
+    ...Platform.select({
+      android: {
+        elevation: 5,
+      },
+      ios: {
+        boxShadow:
+          '0px 46px 13px 0px rgba(23, 84, 46, 0.00), 0px 29px 12px 0px rgba(23, 84, 46, 0.02), 0px 17px 10px 0px rgba(23, 84, 46, 0.08), 0px 7px 7px 0px rgba(23, 84, 46, 0.13), 0px 2px 4px 0px rgba(23, 84, 46, 0.15)',
+      },
+    }),
   },
   title: {
     color: colors.gray[100],

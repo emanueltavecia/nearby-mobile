ee925c3c3527e189ab21ff4b720e91baa8093bd7
Emanuel Tavecia <emanueltavecia@hotmail.com>
2024-12-12 02:04:01 -0300

    add map to home screen, current location functionality and display markers on map

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -3,9 +3,12 @@ import { Categories } from '@/components/categories'
 import { Category } from '@/components/categories/types'
 import { Place } from '@/components/place/types'
 import { Places } from '@/components/places'
+import { colors, fontFamily } from '@/styles/theme'
+import * as Location from 'expo-location'
+import { router } from 'expo-router'
 import { useEffect, useState } from 'react'
-import { Alert, View } from 'react-native'
-import MapView from 'react-native-maps'
+import { Alert, Text, View } from 'react-native'
+import MapView, { Callout, Marker } from 'react-native-maps'
 
 const currentLocation = {
   latitude: -23.561187293883442,
@@ -13,6 +16,8 @@ const currentLocation = {
 }
 
 export default function Home() {
+  const [location, setLocation] = useState<Location.LocationObject | null>(null)
+
   const [categories, setCategories] = useState<Category[]>([])
   const [selectedCategory, setSelectedCategory] = useState('')
 
@@ -43,7 +48,21 @@ export default function Home() {
     }
   }
 
+  async function getCurrentLocation() {
+    try {
+      let { granted } = await Location.requestForegroundPermissionsAsync()
+      if (granted) {
+        const location = await Location.getCurrentPositionAsync({})
+        setLocation(location)
+      }
+    } catch (error) {
+      console.error(error)
+      Alert.alert('Localização', 'Não foi possível obter sua localização.')
+    }
+  }
+
   useEffect(() => {
+    getCurrentLocation()
     fetchCategories()
   }, [])
 
@@ -67,7 +86,51 @@ export default function Home() {
           latitudeDelta: 0.01,
           longitudeDelta: 0.01,
         }}
-      />
+      >
+        <Marker
+          identifier="current"
+          coordinate={{
+            latitude: currentLocation.latitude,
+            longitude: currentLocation.longitude,
+          }}
+          image={require('@/assets/location.png')}
+        />
+
+        {markets.map((market) => (
+          <Marker
+            key={market.id}
+            identifier={market.id}
+            coordinate={{
+              latitude: market.latitude,
+              longitude: market.longitude,
+            }}
+            image={require('@/assets/pin.png')}
+          >
+            <Callout onPress={() => router.navigate(`/market/${market.id}`)}>
+              <View>
+                <Text
+                  style={{
+                    fontSize: 14,
+                    color: colors.gray[600],
+                    fontFamily: fontFamily.medium,
+                  }}
+                >
+                  {market.name}
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 12,
+                    color: colors.gray[600],
+                    fontFamily: fontFamily.regular,
+                  }}
+                >
+                  {market.address}
+                </Text>
+              </View>
+            </Callout>
+          </Marker>
+        ))}
+      </MapView>
 
       <Places data={markets} />
     </View>

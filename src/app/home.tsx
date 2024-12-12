0f58f976e2d9f6a957beb2e6c10fe76a8eba3ed5
Emanuel Tavecia <emanueltavecia@hotmail.com>
2024-12-11 23:55:28 -0300

    add react-native-maps dependency and integrate MapView in home component

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -5,6 +5,12 @@ import { Place } from '@/components/place/types'
 import { Places } from '@/components/places'
 import { useEffect, useState } from 'react'
 import { Alert, View } from 'react-native'
+import MapView from 'react-native-maps'
+
+const currentLocation = {
+  latitude: -23.561187293883442,
+  longitude: -46.656451388116494,
+}
 
 export default function Home() {
   const [categories, setCategories] = useState<Category[]>([])
@@ -51,6 +57,16 @@ export default function Home() {
         data={categories}
         selectedCategory={selectedCategory}
         onSelectCategory={setSelectedCategory}
+      />
+
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: currentLocation.latitude,
+          longitude: currentLocation.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
       />
 
       <Places data={markets} />
